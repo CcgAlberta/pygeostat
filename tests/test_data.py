efcb427bc660f37860e331781aa4e7e0e7d27a38
Zahra Hadavand <zahra.hadavand@gmail.com>
2021-06-10 13:05:37 -0600

    some minor edits to the tests.

--- tests/test_data.py
+++ tests/test_data.py
@@ -18,87 +18,87 @@ import warnings
 import subprocess
 
 
-# class PygeostatDataFileTest(unittest.TestCase):
-#     '''
-#      Test suite for pygeostat data file class
-#     '''
+class PygeostatDataFileTest(unittest.TestCase):
+    '''
+     Test suite for pygeostat data file class
+    '''
 
-#     def setUp(self):
-#         warnings.simplefilter('ignore', category=ImportWarning)
-
-
-#     def tearDown(self):
-#         for file in os.listdir('.'):
-#             _, extention = os.path.splitext(file)
-#             if extention in ['.vtk', '.h5', '.vtu']:
-#                 os.remove(file)
-
-#     def test_file_not_found(self):
-#         with self.assertRaises(FileNotFoundError):
-#             _ = gs.DataFile(flname='notfound')
-
-#     def test_load_gslib_attributes(self):
-#         dat = gs.ExampleData('oilsands')
-#         self.assertEqual(dat.x.lower(), 'east')
-#         self.assertEqual(dat.y.lower(), 'north')
-#         self.assertEqual(dat.z.lower(), 'elevation')
-
-#     def test_read_csv(self):
-#         try:
-#             dat= gs.read_file('https://people.sc.fsu.edu/~jburkardt/data/csv/addresses.csv')
-#         except Exception:
-#             self.fail('Unable to load the CSV file')
-
-#     def test_write_read_h5(self):
-#         data = {'East': [1000, 1200], 'North': [2000, 2500]}
-#         dat = gs.DataFile(data=data)
-
-#         try:
-#             dat.write_file('test.h5')
-#         except Exception:
-#             self.fail('Unable to write hdf5 output')
-
-#     def test_write_vtk(self):
-#         data = {'East': [1000, 1200], 'North': [2000, 2500], 'Elevation': [1,1]}
-#         dat = gs.DataFile(data=data, dftype='point')
-
-#         try:
-#             dat.write_file('test.vtk')
-#         except Exception:
-#             self.fail('Unable to write vtk output')
-
-#     def test_set_catdict(self):
-#         data = {'East': [1000, 1200], 'North': [2000, 2500], 'Elevation': [1,1], 'Categorical': [0,1]}
-#         dat = gs.DataFile(data=data, dftype='point', cat='Categorical')
-
-#         try:
-#             dat.setcatdict({0: 'Category 1', 1: 'Category 2'})
-#         except Exception:
-#             self.fail('Unable to set the categorical dictionary')
-
-#     def test_set_catdict_missing_category(self):
-#         '''
-#         A test to check a missing categorical code in a dictionary raises the correct exception
-#         '''
-
-#         data = {'East': [1000, 1200, 1500], 'North': [2000, 2500, 2600], 'Elevation': [1,1, 2], 'Categorical': [0,1,-99]}
-#         dat = gs.DataFile(data=data, dftype='point', cat='Categorical')
-
-#         with self.assertRaises(ValueError):
-#             dat.setcatdict({0: 'Category 1', 1: 'Category 2'})
-
-#     def test_set_catdict_typeissue(self):
-#         '''
-#         A test to check that provided non numeric codes can be handled by a proper error message
-#         '''
-#         data = {'East': [1000, 1200, 1500], 'North': [2000, 2500, 2600], 'Elevation': [1,1, 2], 'Categorical': [0,1,'-99']}
-#         dat = gs.DataFile(data=data, dftype='point', cat='Categorical')
-
-#         with self.assertRaises(TypeError):
-#             dat.setcatdict({0: 'Category 1', 1: 'Category 2', '-99': 'Unknown'})
+    def setUp(self):
+        warnings.simplefilter('ignore', category=ImportWarning)
 
 
-class PygeostatDataFile_infergriddef_Test(unittest.TestCase):
+    def tearDown(self):
+        for file in os.listdir('.'):
+            _, extention = os.path.splitext(file)
+            if extention in ['.vtk', '.h5', '.vtu']:
+                os.remove(file)
+
+    def test_file_not_found(self):
+        with self.assertRaises(FileNotFoundError):
+            _ = gs.DataFile(flname='notfound')
+
+    def test_load_gslib_attributes(self):
+        dat = gs.ExampleData('oilsands')
+        self.assertEqual(dat.x.lower(), 'east')
+        self.assertEqual(dat.y.lower(), 'north')
+        self.assertEqual(dat.z.lower(), 'elevation')
+
+    def test_read_csv(self):
+        try:
+            dat= gs.read_file('https://people.sc.fsu.edu/~jburkardt/data/csv/addresses.csv')
+        except Exception:
+            self.fail('Unable to load the CSV file')
+
+    def test_write_read_h5(self):
+        data = {'East': [1000, 1200], 'North': [2000, 2500]}
+        dat = gs.DataFile(data=data)
+
+        try:
+            dat.write_file('test.h5')
+        except Exception:
+            self.fail('Unable to write hdf5 output')
+
+    def test_write_vtk(self):
+        data = {'East': [1000, 1200], 'North': [2000, 2500], 'Elevation': [1,1]}
+        dat = gs.DataFile(data=data, dftype='point')
+
+        try:
+            dat.write_file('test.vtk')
+        except Exception:
+            self.fail('Unable to write vtk output')
+
+    def test_set_catdict(self):
+        data = {'East': [1000, 1200], 'North': [2000, 2500], 'Elevation': [1,1], 'Categorical': [0,1]}
+        dat = gs.DataFile(data=data, dftype='point', cat='Categorical')
+
+        try:
+            dat.setcatdict({0: 'Category 1', 1: 'Category 2'})
+        except Exception:
+            self.fail('Unable to set the categorical dictionary')
+
+    def test_set_catdict_missing_category(self):
+        '''
+        A test to check a missing categorical code in a dictionary raises the correct exception
+        '''
+
+        data = {'East': [1000, 1200, 1500], 'North': [2000, 2500, 2600], 'Elevation': [1,1, 2], 'Categorical': [0,1,-99]}
+        dat = gs.DataFile(data=data, dftype='point', cat='Categorical')
+
+        with self.assertRaises(ValueError):
+            dat.setcatdict({0: 'Category 1', 1: 'Category 2'})
+
+    def test_set_catdict_typeissue(self):
+        '''
+        A test to check that provided non numeric codes can be handled by a proper error message
+        '''
+        data = {'East': [1000, 1200, 1500], 'North': [2000, 2500, 2600], 'Elevation': [1,1, 2], 'Categorical': [0,1,'-99']}
+        dat = gs.DataFile(data=data, dftype='point', cat='Categorical')
+
+        with self.assertRaises(TypeError):
+            dat.setcatdict({0: 'Category 1', 1: 'Category 2', '-99': 'Unknown'})
+
+
+class PygeostatDataFileInfergriddefTest(unittest.TestCase):
     '''
      Test suite for pygeostat infergriddef of data file class
     '''
@@ -139,7 +139,7 @@ class PygeostatDataFile_infergriddef_Test(unittest.TestCase):
         self.compare(df3d.infergriddef(blksize = [50,60,1]),
                      df3d.infergriddef(blksize = [50,60,0]))             
                            
-        # nz given as None or 0 or 1 returns a 2d grid that covers the vertical extent of a 3D dataset.
+        # nz given as None or 0 or 1 returns a 2D grid that covers the vertical extent of a 3D dataset.
         self.compare(df3d.infergriddef(nblk = [50,60,1]),
                      gs.GridDef('''50  119.8   19.6 
                                    60  1209.1  18.2 
@@ -157,7 +157,7 @@ class PygeostatDataFile_infergriddef_Test(unittest.TestCase):
         self.compare(df3d.infergriddef(nblk=_nblk),
                      df3d.infergriddef(nblk=[int(i)  for i in _nblk]))
 
-        # If the grid is 2-D, zsiz must be provided as None. Otherwise it must raise exception.
+        # If data is 2-D, zsiz must be provided as None. Otherwise it must raise exception.
         with self.assertRaises(ValueError):
             df2d.infergriddef(blksize = [50,60,4])
 
@@ -169,7 +169,7 @@ class PygeostatDataFile_infergriddef_Test(unittest.TestCase):
                                    19   1230.0  60.0 
                                    1    0.5     1.0'''))   
 
-        #If the grid is 2-D, nz must be provided as None. Otherwise it must raise exception.
+        #If data is 2-D, nz must be provided as None. Otherwise it must raise exception.
         with self.assertRaises(ValueError):
             df2d.infergriddef(nblk = [50,60,1])       
 
@@ -183,88 +183,88 @@ class PygeostatDataFile_infergriddef_Test(unittest.TestCase):
 
 
 
-# class PygeostatGridDefFromString(unittest.TestCase):
-#     '''
-#      Test suite for pygeostat GridDef class with grid string
-#     '''
+class PygeostatGridDefFromString(unittest.TestCase):
+    '''
+     Test suite for pygeostat GridDef class with grid string
+    '''
 
-#     def setUp(self):
-#         warnings.simplefilter('ignore', category=ImportWarning)
-#         self.grid = gs.GridDef('''40  0.5  1
-#                      40  0.5  1
-#                      40  0.5  1''')
-#         self.testfl = gs.ExampleData('3d_grid', griddef=self.grid)
+    def setUp(self):
+        warnings.simplefilter('ignore', category=ImportWarning)
+        self.grid = gs.GridDef('''40  0.5  1
+                     40  0.5  1
+                     40  0.5  1''')
+        self.testfl = gs.ExampleData('3d_grid', griddef=self.grid)
 
-#     def test_grid_count(self):
-#         assert self.testfl.griddef.count() == 64000
-#         self.assertEqual(self.grid.count(), 64000)
+    def test_grid_count(self):
+        assert self.testfl.griddef.count() == 64000
+        self.assertEqual(self.grid.count(), 64000)
 
-#     def test_grid_origin(self):
-#         assert self.testfl.griddef.origin() == (0.0, 0.0, 0.0)
-#         assert self.grid.origin() == (0.0, 0.0, 0.0)
+    def test_grid_origin(self):
+        assert self.testfl.griddef.origin() == (0.0, 0.0, 0.0)
+        assert self.grid.origin() == (0.0, 0.0, 0.0)
 
-#     def test_grid_blockvolume(self):
-#         assert self.testfl.griddef.block_volume == 1.0
-#         assert self.grid.block_volume == 1.0
+    def test_grid_blockvolume(self):
+        assert self.testfl.griddef.block_volume == 1.0
+        assert self.grid.block_volume == 1.0
 
-#     def test_grid_extents(self):
-#         assert self.testfl.griddef.extents() == [(0.0, 40.0), (0.0, 40.0), (0.0, 40.0)]
-#         assert self.grid.extents() == [(0.0, 40.0), (0.0, 40.0), (0.0, 40.0)]
+    def test_grid_extents(self):
+        assert self.testfl.griddef.extents() == [(0.0, 40.0), (0.0, 40.0), (0.0, 40.0)]
+        assert self.grid.extents() == [(0.0, 40.0), (0.0, 40.0), (0.0, 40.0)]
 
-#     def test_grid_indexcoords(self):
-#         assert self.testfl.griddef.get_index(10.0, 5.0, 15.0) == (22569, True)
-#         assert self.grid.get_index(10.0, 5.0, 15.0) == (22569, True)
-#         # Check for coordinates outside of grid
-#         assert self.testfl.griddef.get_index(10.0, 5.0, 50.0) == (-1, False)
-#         assert self.grid.get_index(10.0, 5.0, 50.0) == (-1, False)
+    def test_grid_indexcoords(self):
+        assert self.testfl.griddef.get_index(10.0, 5.0, 15.0) == (22569, True)
+        assert self.grid.get_index(10.0, 5.0, 15.0) == (22569, True)
+        # Check for coordinates outside of grid
+        assert self.testfl.griddef.get_index(10.0, 5.0, 50.0) == (-1, False)
+        assert self.grid.get_index(10.0, 5.0, 50.0) == (-1, False)
 
-#     def test_grid_indicescoords(self):
-#         assert self.testfl.griddef.get_index3d(10.0, 5.0, 15.0) == (9, 4, 14, True)
-#         assert self.grid.get_index3d(10.0, 5.0, 15.0) == (9, 4, 14, True)
-#         # Check for coordinates outside of grid
-#         assert self.testfl.griddef.get_index3d(10.0, 5.0, 50.0) == (-1, -1, -1, False)
-#         assert self.grid.get_index3d(10.0, 5.0, 50.0) == (-1, -1, -1, False)
+    def test_grid_indicescoords(self):
+        assert self.testfl.griddef.get_index3d(10.0, 5.0, 15.0) == (9, 4, 14, True)
+        assert self.grid.get_index3d(10.0, 5.0, 15.0) == (9, 4, 14, True)
+        # Check for coordinates outside of grid
+        assert self.testfl.griddef.get_index3d(10.0, 5.0, 50.0) == (-1, -1, -1, False)
+        assert self.grid.get_index3d(10.0, 5.0, 50.0) == (-1, -1, -1, False)
 
 
-# class PygeostatGridDefFromArray(unittest.TestCase):
-#     '''
-#      Test suite for pygeostat GridDef class with grid array
-#     '''
+class PygeostatGridDefFromArray(unittest.TestCase):
+    '''
+     Test suite for pygeostat GridDef class with grid array
+    '''
 
-#     def setUp(self):
-#         warnings.simplefilter('ignore', category=ImportWarning)
-#         self.grid = gs.GridDef([40,0.5,1,40,0.5,1,40,0.5,1])
-#         self.testfl = gs.ExampleData('3d_grid', griddef=self.grid)
+    def setUp(self):
+        warnings.simplefilter('ignore', category=ImportWarning)
+        self.grid = gs.GridDef([40,0.5,1,40,0.5,1,40,0.5,1])
+        self.testfl = gs.ExampleData('3d_grid', griddef=self.grid)
 
-#     def test_grid_count(self):
-#         assert self.testfl.griddef.count() == 64000
-#         self.assertEqual(self.grid.count(), 64000)
+    def test_grid_count(self):
+        assert self.testfl.griddef.count() == 64000
+        self.assertEqual(self.grid.count(), 64000)
 
-#     def test_grid_origin(self):
-#         assert self.testfl.griddef.origin() == (0.0, 0.0, 0.0)
-#         assert self.grid.origin() == (0.0, 0.0, 0.0)
+    def test_grid_origin(self):
+        assert self.testfl.griddef.origin() == (0.0, 0.0, 0.0)
+        assert self.grid.origin() == (0.0, 0.0, 0.0)
 
-#     def test_grid_blockvolume(self):
-#         assert self.testfl.griddef.block_volume == 1.0
-#         assert self.grid.block_volume == 1.0
+    def test_grid_blockvolume(self):
+        assert self.testfl.griddef.block_volume == 1.0
+        assert self.grid.block_volume == 1.0
 
-#     def test_grid_extents(self):
-#         assert self.testfl.griddef.extents() == [(0.0, 40.0), (0.0, 40.0), (0.0, 40.0)]
-#         assert self.grid.extents() == [(0.0, 40.0), (0.0, 40.0), (0.0, 40.0)]
+    def test_grid_extents(self):
+        assert self.testfl.griddef.extents() == [(0.0, 40.0), (0.0, 40.0), (0.0, 40.0)]
+        assert self.grid.extents() == [(0.0, 40.0), (0.0, 40.0), (0.0, 40.0)]
 
-#     def test_grid_indexcoords(self):
-#         assert self.testfl.griddef.get_index(10.0, 5.0, 15.0) == (22569, True)
-#         assert self.grid.get_index(10.0, 5.0, 15.0) == (22569, True)
-#         # Check for coordinates outside of grid
-#         assert self.testfl.griddef.get_index(10.0, 5.0, 50.0) == (-1, False)
-#         assert self.grid.get_index(10.0, 5.0, 50.0) == (-1, False)
+    def test_grid_indexcoords(self):
+        assert self.testfl.griddef.get_index(10.0, 5.0, 15.0) == (22569, True)
+        assert self.grid.get_index(10.0, 5.0, 15.0) == (22569, True)
+        # Check for coordinates outside of grid
+        assert self.testfl.griddef.get_index(10.0, 5.0, 50.0) == (-1, False)
+        assert self.grid.get_index(10.0, 5.0, 50.0) == (-1, False)
 
-#     def test_grid_indicescoords(self):
-#         assert self.testfl.griddef.get_index3d(10.0, 5.0, 15.0) == (9, 4, 14, True)
-#         assert self.grid.get_index3d(10.0, 5.0, 15.0) == (9, 4, 14, True)
-#         # Check for coordinates outside of grid
-#         assert self.testfl.griddef.get_index3d(10.0, 5.0, 50.0) == (-1, -1, -1, False)
-#         assert self.grid.get_index3d(10.0, 5.0, 50.0) == (-1, -1, -1, False)
+    def test_grid_indicescoords(self):
+        assert self.testfl.griddef.get_index3d(10.0, 5.0, 15.0) == (9, 4, 14, True)
+        assert self.grid.get_index3d(10.0, 5.0, 15.0) == (9, 4, 14, True)
+        # Check for coordinates outside of grid
+        assert self.testfl.griddef.get_index3d(10.0, 5.0, 50.0) == (-1, -1, -1, False)
+        assert self.grid.get_index3d(10.0, 5.0, 50.0) == (-1, -1, -1, False)
 
 
 
