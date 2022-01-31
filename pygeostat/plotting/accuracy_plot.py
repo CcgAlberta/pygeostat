8f83d79fd7ae6c8a383c17a1e2060a1114188f5f
Zahra Hadavand <zahra.hadavand@gmail.com>
2022-01-30 22:17:37 -0700

    adding some clarification to accuracy plot docstring.

--- pygeostat/plotting/accuracy_plot.py
+++ pygeostat/plotting/accuracy_plot.py
@@ -73,10 +73,19 @@ def accuracy_plot(truth=None, reals=None, mik_thresholds=None, acctype='sim', pr
 	Returns:
 		ax (ax): Matplotlib Axes object with the cross validation plot
 
+
+	Note: 
+		The simulated values in each row of “reals” are used to calculate an empirical CDF using the standard method of 
+		the midpoint of a histogram bin and the CDF is constructed using all points (as done in GSLIB). When "mik_thresholds" is None, 
+		“reals” contains the simulated values from which a cdf should be computed. Otherwise the “reals” contains the distribution values, 
+		F(mik_thresholds), for each location (each row of the data file).
+
+
 	Examples:
 
 
-	A simple call using truth and realization data:
+	A simple call using truth and realization data: in this example, the first column of data_file is "truth" 
+	and the rest of columns are "reals".
 
 	.. plot::
 
