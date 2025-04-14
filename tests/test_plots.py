3cc8760520583258d7879461da55975083257ceb
ronnyhdez <ronny.hernandezm@gmail.com>
2025-04-14 13:31:09 -0600

    Ref #81 plots tests

--- tests/test_plots.py
+++ tests/test_plots.py
@@ -4,12 +4,9 @@
 """Tests for plotting functionality in pygeostat"""
 
 import os
-import sys
 import warnings
 import tempfile
 import pytest
-import numpy as np
-import pandas as pd
 import pygeostat as gs
 from matplotlib import pyplot as plt
 
@@ -28,6 +25,13 @@ def location_plot_data():
     """Provide plot sample data"""
     return gs.ExampleData('point3d_ind_mv')
 
+@pytest.fixture
+def accuracy_plot_data():
+    """Prepare data for the accuracy plots."""
+    data = gs.ExampleData('accuracy_plot')
+    reals = data[list(data.columns[1:])].values
+    truth = data[list(data.columns[0:])].values
+    return truth, reals
 
 def test_location_plot_simple(plot_output_dir, location_plot_data):
     """Test capability of plotting by passing just the data file"""
@@ -73,4 +77,46 @@ def test_location_plot_lines(plot_output_dir, location_plot_data):
 
     assert os.path.isfile(output_file)
 
+def test_location_plot_categorical(plot_output_dir, location_plot_data):
+    """Test plotting categorical variables along xz/yz orientation."""
+    output_file = os.path.join(plot_output_dir, 'location_plot_categorical_test.png')
+    _, ax = plt.subplots(1, 1)
+    
+    try:
+        _ = gs.location_plot(
+            location_plot_data, 
+            var='Lithofacies', 
+            orient='yz', 
+            aspect=5, 
+            plot_collar=True, 
+            output_file=output_file
+        )
+        _ = gs.location_plot(
+            location_plot_data, 
+            var='Lithofacies', 
+            orient='yz', 
+            aspect=5, 
+            plot_collar=True, 
+            output_file=output_file, 
+            ax=ax
+        )
+    except Exception as ex:
+        pytest.fail(f'Unable to test the location plot for a categorical variable \n{str(ex)}')
+
+    assert os.path.isfile(output_file)
+
+
+def test_accuracy_plot(plot_output_dir, accuracy_plot_data):
+    """Test the capability of plotting the accuracy with no function parameters"""
+    truth, reals = accuracy_plot_data
+    output_file = os.path.join(plot_output_dir, 'accuracy_plot_test.png')
+    _, ax = plt.subplots(1,1)
+
+    try:
+        gs.accuracy_plot(truth=truth, reals=reals, output_file=output_file)
+        gs.accuracy_plot(truth=truth, reals=reals, output_file=output_file, ax=ax)
+    except Exception as ex:
+        pytest.fail(f'Unable to test the accuracy plot with simple settings \n{str(ex)}')
+
+    assert os.path.isfile(output_file)
 
