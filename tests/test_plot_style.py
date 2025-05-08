3004b811fa395a0c3bb36674f5f00382e92b1704
ronnyhdez <ronny.hernandezm@gmail.com>
2025-05-08 12:10:07 -0600

    Ref #90 first test and imports working

--- tests/test_plot_style.py
+++ tests/test_plot_style.py
@@ -20,54 +20,67 @@ def plot_style():
     return PlotStyle
 
 
+def test_update_key(plot_style):
+    """Test to update plot style parameters with valid keys."""
+
+    # Original so it can be restored later
+    original_value = plot_style.get('axes.grid', None)
+
+    try:
+        # Update with valid key
+        plot_style.update({'axes.grid': True})
+        assert plot_style['axes.grid'] is True
+    finally:
+        # Restore orignal value to avoid affect rest of tests
+        if original_value is not None:
+            plot_style.update({'axes.grid': original_value})
 
 
 
 
-
-class PygeostatPlotStyleTest(unittest.TestCase):
-    '''
-     Test suite for pygeostat plot style settings
-    '''
-
-    def setUp(self):
-        warnings.simplefilter('ignore', category=ImportWarning)
-        self.plot_style = gs.PlotStyle
-
-
-    def test_update_key(self):
-        '''
-        A collection of test to check the update behavior for pygeostat Parameters object
-        '''
-
-        # Correct assignment
-        self.plot_style.update({'axes.grid': True})
-        
-        self.assertEqual(self.plot_style['axes.grid'], True)
-
-        # Wrong Key
-        with self.assertRaises(KeyError):
-            self.plot_style.update({'FakeParameters':0})
-    
-
-    def test_set_default_plot_style(self):
-        '''
-        Test the capability of writing pygeostat plot styles into a config file under the user folder
-        '''
-        try:
-            self.plot_style.set_systemdefault()
-        except Exception as ex:
-            self.fail('Unable to set system defaults \n{}'.format(str(ex)))
-
-    def test_get_default_plot_style(self):
-        '''
-        test getting the default plot style
-        '''
-
-        try:
-            self.plot_style.get_systemdefault()
-        except Exception as ex:
-            self.fail('Unable to set system defaults \n{}'.format(str(ex)))
-
-if __name__ == '__main__':
-	subprocess.call([sys.executable, '-m', 'unittest', str(__file__), '-v'])
+#class PygeostatPlotStyleTest(unittest.TestCase):
+#    '''
+#     Test suite for pygeostat plot style settings
+#    '''
+#
+#    def setUp(self):
+#        warnings.simplefilter('ignore', category=ImportWarning)
+#        self.plot_style = gs.PlotStyle
+#
+#
+#    def test_update_key(self):
+#        '''
+#        A collection of test to check the update behavior for pygeostat Parameters object
+#        '''
+#
+#        # Correct assignment
+#        self.plot_style.update({'axes.grid': True})
+#        
+#        self.assertEqual(self.plot_style['axes.grid'], True)
+#
+#        # Wrong Key
+#        with self.assertRaises(KeyError):
+#            self.plot_style.update({'FakeParameters':0})
+#    
+#
+#    def test_set_default_plot_style(self):
+#        '''
+#        Test the capability of writing pygeostat plot styles into a config file under the user folder
+#        '''
+#        try:
+#            self.plot_style.set_systemdefault()
+#        except Exception as ex:
+#            self.fail('Unable to set system defaults \n{}'.format(str(ex)))
+#
+#    def test_get_default_plot_style(self):
+#        '''
+#        test getting the default plot style
+#        '''
+#
+#        try:
+#            self.plot_style.get_systemdefault()
+#        except Exception as ex:
+#            self.fail('Unable to set system defaults \n{}'.format(str(ex)))
+#
+#if __name__ == '__main__':
+#	subprocess.call([sys.executable, '-m', 'unittest', str(__file__), '-v'])
